d383178091d7e2f957f6bdaa15e3ef69bfc211b3
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-06 00:27:34 +0300

    We then initialize the Bootstrap class by passing in the app instance.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask 
 from .config import DevConfig 
+from flask_bootstrap import Boostrap
 
 app=Flask(__name__,instance_relative_config=True)
 
  #setting up configurations 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')          
+
+
+#initializing flask extensions
+bootstrap =Boostrap(app)
 
 from app import views    
 
