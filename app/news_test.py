430cfdaa391291e9747f6d2ce81e50361e1e7c94
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-05 18:12:18 +0300

    adds access to app configuration objects

--- app/news_test.py
+++ app/news_test.py
@@ -2,16 +2,16 @@ import unittest
 from models import source
 from models import article
 Source=source.Source
-article=article.Article
+Article=article.Article
 
-class SourceTest(unnittest.TestCase):
+class SourceTest(unittest.TestCase):
     '''
     Test class to test the behavior of the source class.
     '''
 
     def setUp(self):
         '''
-        set up method that will rrun before every test 
+        set up method that will run before every test 
         '''
         self.new_source=Source("abc","Abc news","this gives timely news","https://www.aljazeera.com/news/","private",)
 
@@ -20,10 +20,15 @@ class SourceTest(unnittest.TestCase):
         self.assertTrue(isinstance(self.new_source,Source))
 
 
-# class ArticleTest(unnittest.TestCase):
-#     '''
-#     Test class to test the behavior of the article class.
-#     '''
+class ArticleTest(unittest.TestCase):
+    '''
+    Test class to test the behavior of the article class.
+    '''
+    def setUp(self):
+        self.new_article=Article("John","mountain goes to the lake", "HEY" ,"the 2022 endpoint","https://www.standardmedia.co.ke/politics/article/2001389638/inside-railas-talks-with-central-elders","2020-2-2","Done deal")
+
+    def  test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
 
 
 
--- app/request.py
+++ app/request.py
@@ -1,5 +1,18 @@
 from app import app
+import urllib.request, json
+from .models import article
+from .models import source 
+
+Article=article.Article
+Source=source.Source
+
 
 #getting api key 
 
 api_key=app.config['NEWS_API_KEY']
+
+#geting base url
+
+source_url=app.config['NEWS_SOURCES_URL']
+source_article_url=app.config['NEWS_SOURCE_API']
+
